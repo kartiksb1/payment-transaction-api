87994ef28382f3c3e58ea8eba93babe0185a05ba
kartiksb1 <biradarkatik15@gmal.com>
2026-01-30 00:23:16 +0530

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/example/payment_transaction_api/PaymentTransactionApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.payment_transaction_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTransactionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentTransactionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/payment_transaction_api/PaymentTransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payment_transaction_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
