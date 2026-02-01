3ae2be7ec97fccad5915d5b1cad054c8ecb88a4d
kartiksb1 <biradarkatik15@gmal.com>
2026-02-01 23:16:19 +0530

    added RollingFileAppender configuration

--- src/main/java/com/example/payment_transaction_api/PaymentTransactionApiApplication.java
+++ src/main/java/com/example/payment_transaction_api/PaymentTransactionApiApplication.java
@@ -9,5 +9,4 @@ public class PaymentTransactionApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentTransactionApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/payment_transaction_api/service/PaymentService.java
+++ src/main/java/com/example/payment_transaction_api/service/PaymentService.java
@@ -4,15 +4,15 @@ package com.example.payment_transaction_api.service;
 import com.example.payment_transaction_api.dto.PaymentRequest;
 import com.example.payment_transaction_api.exception.InsufficientBalanceException;
 import com.example.payment_transaction_api.exception.PaymentFailedException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
-
-    private static final Logger logger = LoggerFactory.getLogger(PaymentService.class);
-
+	
+	private static Logger logger=LoggerFactory.getLogger(PaymentService.class);
     public void makePayment(PaymentRequest request) {
         logger.info("Payment initiated: from={} to={} amount={}", 
                     request.getFromAccount(), request.getToAccount(), request.getAmount());
